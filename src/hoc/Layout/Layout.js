f547003b13ac9193a774cea8e7716c2884caf085
Listopad02 <123ayetvbass@gmail.com>
2022-03-15 15:35:56 +0300

    initial commit

---
+++ src/hoc/Layout/Layout.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Layout = ({ children }) => {
+  return (
+    <div>
+        <header>
+
+        </header>
+        <main>
+            { children }
+        </main>
+    </div>
+  )
+}
+
+export default Layout
